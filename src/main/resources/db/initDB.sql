213cc0f2c09554371c7f99abe2190595768255e5
Andrey <avpoponin@yandex.ru>
2018-07-21 22:27:53 +0300

    Add solution HW3

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -1,3 +1,4 @@
+DROP TABLE IF EXISTS meals;
 DROP TABLE IF EXISTS user_roles;
 DROP TABLE IF EXISTS users;
 DROP SEQUENCE IF EXISTS global_seq;
@@ -22,4 +23,15 @@ CREATE TABLE user_roles
   role    VARCHAR,
   CONSTRAINT user_roles_idx UNIQUE (user_id, role),
   FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
+
+CREATE  TABLE meals
+(
+  user_id INTEGER NOT NULL,
+  id INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+  datetime TIMESTAMP NOT NULL,
+  description VARCHAR NOT NULL,
+  calories INTEGER NOT NULL,
+  CONSTRAINT meals_idx UNIQUE (id),
+  FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
